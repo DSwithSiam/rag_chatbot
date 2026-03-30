fcab48be52dcd2f7d83cb4fb065bbcf5ea605e07
Md Masipul Islam Siam <masipulislam@gmail.com>
2026-03-30 17:02:35 +0600

    update URL routing in chatbot application

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import ChatView, ChatMessageView
 
 urlpatterns = [
-    
+    path('chat/', ChatView.as_view(), name='chat'),
+    path('messages/', ChatMessageView.as_view(), name='chatmessage'),
 ]
